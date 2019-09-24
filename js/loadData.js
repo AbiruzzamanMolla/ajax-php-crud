f412f419dc92e9d7e353ff3ffbe9b42586921087
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-09-24 14:11:35 +0600

    deleted unnecessary files

--- js/loadData.js
+++
@@ -1,9 +0,0 @@
-function loadData() {
-    $.ajax({
-        url: 'bend/read.php',
-        method: 'GET',
-        success: function (responce) {
-            console.log(responce);
-        }
-    });
-}
